99eef3403a34c396cfb2e4f31e55d767987685d2
cwan2873 <727423281@qq.com>
2023-09-22 18:50:12 +1000

    create a new repository

--- FindLensTemplate/FindLensTemplate.py
+++ FindLensTemplate/FindLensTemplate.py
@@ -1,15 +1,31 @@
 import numpy as np
-from cv2 import matchTemplate, TM_CCOEFF_NORMED
+import cv2 as cv
 from astropy.io import fits
+import os
 
 
 class FindLensTemplate():
+    """
+    This class is a quick method to find lens template in other waveband fits files.
+    """
 
     def __int__(self, target_path):
+        """
+        :param target_path: The directory saves the lens fits files
+        :return:
+        """
 
         self.image_path_list = target_path
 
-    def MethodMathch(self, image_path, temp, method=TM_CCOEFF_NORMED, isMax=True):
+    def MethodMathch(self, image_path, temp, method=cv.TM_CCOEFF_NORMED, isMax=True):
+        """
+        A simple template matching gravitational lens
+        :param image_path: the image path
+        :param temp:
+        :param method:
+        :param isMax:
+        :return:
+        """
 
         # open the image fits file
         image_file = fits.open(image_path)
@@ -19,42 +35,54 @@ class FindLensTemplate():
         # Apply template Matching
         if isinstance(temp, str):
             temp = fits.open(temp)[0].data
-            res = matchTemplate(img, temp, method=method)
+            res = cv.matchTemplate(img, temp, method=method)
 
         elif isinstance(temp, np.ndarray):
 
-            res = matchTemplate(img, temp, method=method)
+            res = cv.matchTemplate(img, temp, method=method)
 
-        min_val, max_val, min_loc, max_loc = minMaxLoc(res)
+        min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
 
         if isMax:
             return max_loc
         else:
             return min_loc
 
-    def MulFileMatch(self, image_list_path):
+    def MulFileMatch(self, image_dir, temp_name="Template.fits", *args, **kwargs):
 
-        for file_path in
+        temp_path = os.path.join(image_dir, temp_name)
 
-        return 0
+        position_list = []
+
+        for file_name in os.listdir(image_dir):
+
+            if file_name != "template.fits":
+
+                file_path = os.path.join(image_dir, file_name)
+
+                position = self.MulFileMatch(file_path, temp_path, kwargs)
+
+                position_list.append(position)
+
+        return np.array(position_list)
 
     def MulMethodMatch(self, image, temp, methods=None, isMax=True):
 
         if methods is None:
-            methods = [TM_CCOEFF_NORMED]
+            methods = [cv.M_CCOEFF_NORMED]
 
         res_ms = []
 
         img = image.copy()
 
         for method in methods:
-            res_m = matchTemplate(img, temp, method=method)
+            res_m = cv.matchTemplate(img, temp, method=method)
 
             res_ms.append(res_m)
 
         res_ms = np.array(res_ms)
 
-        min_val, max_val, min_loc, max_loc = minMaxLoc(res_ms)
+        min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res_ms)
 
         if isMax:
             return max_loc
