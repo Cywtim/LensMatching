288e0e537429101ef9877eaf8ed139e656e19558
cwan2873 <727423281@qq.com>
2023-09-26 16:20:00 +1000

    Cyan: update the visualization.

--- FindLensTemplate/VisualLensTemplate.py
+++ FindLensTemplate/VisualLensTemplate.py
@@ -6,7 +6,7 @@ from matplotlib import pyplot as plt
 from FindLensTemplate import FindLensTemplate
 
 class VisualTemp:
-    def __init__(self, data_dir, position_dict, ncols, nrows, w=50, h=50, figsize=(8, 6), **kwargs):
+    def __init__(self, data_dir, position_dict, ncols, nrows, w=50, h=50, **kwargs):
 
         self.data_dir = data_dir
         self.position_dict = position_dict
@@ -15,18 +15,17 @@ class VisualTemp:
         self.h = h
         self.ncols = ncols
         self.nrows = nrows
-        self.figsize = figsize
 
         if self.len > (self.ncols*self.nrows):
             print("Error, The lens of position dictionary is out of range of subplots.")
             exit()
 
-    def ShowSubplots(self, cmap="rainbow", dotcolor="red"):
+    def ShowSubplots(self, cmap="rainbow", figsize=(8, 6)):
 
         file_name_list = list(self.position_dict)
         position_list = list(self.position_dict.values())
 
-        fig, ax = plt.subplots(self.nrows, self.ncols, figsize=self.figsize)
+        fig, ax = plt.subplots(self.nrows, self.ncols, figsize=figsize)
 
         for r in range(self.nrows):
             for c in range(self.ncols):
@@ -39,6 +38,31 @@ class VisualTemp:
                                     position[0]:position[0]+self.h], cmap=cmap)
                     ax[r, c].set_title(file_name_list[c+self.ncols*r])
                     fig.colorbar(img)
+                except:
+                    pass
+        fig.show()
+
+        return fig, ax
+
+
+    def ShowPosition(self, cmap="rainbow", dotcolor="red", figsize=(6, 10)):
+
+        file_name_list = list(self.position_dict)
+        position_list = list(self.position_dict.values())
+
+        fig, ax = plt.subplots(self.nrows, self.ncols, figsize=figsize)
+
+        for r in range(self.nrows):
+            for c in range(self.ncols):
+                try:
+                    image = fits.open(os.path.join(self.data_dir,
+                                file_name_list[c+self.ncols*r]))
+                    image = image[0].data
+                    position = position_list[c+self.ncols*r]
+                    img = ax[r, c].imshow(image, cmap=cmap)
+                    ax[r, c].scatter(position[1], position[0], color=dotcolor)
+                    ax[r, c].set_title(file_name_list[c+self.ncols*r])
+
                 except:
                     pass
         fig.show()
